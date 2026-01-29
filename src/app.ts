796f0402809d354a766f33e991193259f7311045
mimgggg4444 <minnki19@gmail.com>
2026-01-29 18:33:25 +0900

    feat: contractDocument 라우트 등록

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 //express 5 버전부터는 try/catch를 사용하지 않아도 알아서 에러를 넘겨줌
 import express from 'express';
 import { errorHandler } from './errors/errorHandler';
+import contractDocumentRoute from './routes/contractDocument.route';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json()); // JSON 요청 바디 파싱
 app.use(express.urlencoded({ extended: true })); // URL-encoded 파싱
 
 // 라우터 설정 (계획서의 routes 폴더 활용)
+app.use('/contractDocuments', contractDocumentRoute);
 
 //에러 핸들러 설정 (반드시 라우터보다 아래에 위치!)
 app.use(errorHandler);
